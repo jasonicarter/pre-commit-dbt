d1472c7ee413a1fcf08210a362abae1c10ec5d20
LEUNG WING YAN <42382599+miki-lwy@users.noreply.github.com>
2022-02-09 19:25:39 +0100

    Update dbt_compile.py

--- pre_commit_dbt/dbt_compile.py
+++ pre_commit_dbt/dbt_compile.py
@@ -25,7 +25,9 @@ def prepare_cmd(
         dbt_models = models
     else:
         dbt_models = paths_to_dbt_models(paths, prefix, postfix)
+       
     cmd = ["dbt", *global_flags, "compile", "-m", *dbt_models, *cmd_flags]
+    print(cmd)
     return cmd
 
 
